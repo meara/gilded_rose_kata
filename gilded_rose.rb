617e6c494583aa2b057a1b8b12c7943ee9574797
meara <mcharnetzki@gmail.com>
2016-04-10 21:22:22 -0500

    reduce nesting, reorg

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,25 +1,26 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0 && item.name != 'Sulfuras, Hand of Ragnaros'
-        item.quality -= 1
-      end
+    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'  && item.name != 'Sulfuras, Hand of Ragnaros'
+      item.quality -= 1 if item.quality > 0
     else
       if item.quality < 50
         item.quality += 1
         if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11 && item.quality < 50
-            item.quality += 1
-          end
-          if item.sell_in < 6 && item.quality < 50
-            item.quality += 1
+          if item.quality < 50
+            if item.sell_in < 6
+              item.quality += 2
+            elsif item.sell_in < 11
+              item.quality += 1
+            end
           end
         end
       end
     end
+
     if item.name != 'Sulfuras, Hand of Ragnaros'
       item.sell_in -= 1
     end
+
     if item.sell_in < 0
       if item.name != "Aged Brie"
         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
@@ -35,6 +36,7 @@ def update_quality(items)
         end
       end
     end
+
   end
 end
 
